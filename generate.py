6fbdd2961365e36a067cae7b241137dd0f27b56c
Adam <84996163+g3y@users.noreply.github.com>
2021-09-06 04:52:21 +0200

    1st commit

---
+++ generate.py
@@ -0,0 +1,22 @@
+digits = '0123456789'
+chars = 'abcdefghijklmn' + \
+        'opqrstuvwxyz'
+up = chars.upper()
+special = '_!$%&?ù'
+all = digits+chars+up+special
+from random import choice
+
+password = ''.join (
+    choice(all) for i in range(9)
+)
+
+f = open('ascii.txt', 'r')
+file_contents = f.read()
+
+
+print("\x1b[1;32m                    ")
+print (file_contents)
+f.close()
+print("\033[0;31m")
+print(password)
+print("\033[0;37;40m")
